fe0c6e61007812e6d11d229309b795c5dd659b17
yebun91 <65695703+yebun91@users.noreply.github.com>
2022-04-26 22:21:38 +0900

    Time: 105 ms, Memory: 44.7 MB - LeetHub

---
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let cnt = 0;
+    for(let i of nums){
+        if(String(i).length % 2 === 0){
+           cnt++;
+       }
+    }
+    return cnt;
+};
