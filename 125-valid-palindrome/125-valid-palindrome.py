1e6d1c47d1e18e854a0c47895f08cc13309b24d4
yebun91 <65695703+yebun91@users.noreply.github.com>
2022-07-12 22:49:32 +0900

    Time: 115 ms (11.99%), Space: 19.4 MB (6.28%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,12 +1,12 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        strings = []
-        for char in s :
-            if char.isalnum():
-              strings.append(char.lower())
-        while len(strings) > 1:
-            if strings.pop(0) != strings.pop():
-              return False
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs: Deque = collections.deque()
+        for char in s : 
+          if char.isalnum():
+            strs.append(char.lower())
+
+        while len(strs) > 1:
+          if strs.popleft() != strs.pop():
+            return False
+
         return True
-        
-        
