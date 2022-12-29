c69fa275abc2ac061ded43df4bf11afcc24c2332
DESKTOP-TOJ2O5O\yujin <yebun91@gmail.com>
2022-12-29 19:50:03 +0900

    섹션3 완료

--- 자바스크립트 알고리즘 문제풀이/섹션3/4.가장_짧은_문자거리.js
+++ 자바스크립트 알고리즘 문제풀이/섹션3/4.가장_짧은_문자거리.js
@@ -1,17 +1,27 @@
 function solution(s, t) {
   let answer = [];
-  // let len = s.length;
-  // let minNum = 0;
+  let len = s.length;
+  let minNum = 0;
 
-  // for (let i = 0; i < len; i++) {
-  //   if (s[i] === t) {
-  //     minNum = 0;
-  //     answer.push(minNum);
-  //   } else {
-  //     minNum++;
-  //     answer.push(minNum);
-  //   }
-  // }
+  for (let i = 0; i < len; i++) {
+    if (s[i] === t) {
+      minNum = 0;
+      answer.push(minNum);
+    } else {
+      minNum++;
+      answer.push(minNum);
+    }
+  }
+  
+  for(let i = len-1; i>=0; i--){
+    if (s[i] === t) {
+      minNum = 0;
+    } else {
+      minNum++;
+      answer[i]=Math.min(answer[i], minNum);
+    }
+  }
+
   return answer;
 }
 
