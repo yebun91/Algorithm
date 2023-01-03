a5cee39ae21cc40306a2d663cb0bb9f7a3a2128f
DESKTOP-TOJ2O5O\yujin <yebun91@gmail.com>
2023-01-03 20:09:42 +0900

    섹션 4 하는중

--- 자바스크립트 알고리즘 문제풀이/섹션4/2.뒤집은_소수.js
+++ 자바스크립트 알고리즘 문제풀이/섹션4/2.뒤집은_소수.js
@@ -1,11 +1,10 @@
 function solution(num) {
   let answer = [];
   // for (x of num) {
-  //   let rev = x.toString().split("").reverse().join("");
-  //   const number = isPrime(parseInt(rev));
-  //   if (number) answer.push(parseInt(rev));
+  //   let rev = Number(x.toString().split("").reverse().join(""));
+  //   if (isPrime(rev)) answer.push(rev);
   // }
-  for (let x of arr) {
+  for (let x of num) {
     let res = 0;
     while (x) {
       let t = x % 10;
@@ -14,21 +13,15 @@ function solution(num) {
     }
     if (isPrime(res)) answer.push(res);
   }
-  function isPrime(num) {
-    if (num === 1) return false;
-    if (num === 2) {
-      return true;
-    }
-    for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
-      if (num % i === 0) {
-        // 한 번이라도 나누어 졌으니 소수가 아니므로 return false
-        return false;
-      }
-    }
-    // 나눠진 수가 없다면 해당 수는 소수이므로 return true
-    return true;
-  }
+
   return answer;
+}
+function isPrime(num) {
+  if (num === 1) return false;
+  for (let i = 2; i < parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
 }
 
 let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
