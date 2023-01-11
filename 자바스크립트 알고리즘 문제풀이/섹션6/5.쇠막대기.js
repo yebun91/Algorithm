a7778457ff0df10c15acbd5ffa758978d860ec19
DESKTOP-TLT7564\choi yujin <yujin.choi@truabutment.com>
2023-01-11 16:59:25 +0900

    섹션6 하는중

--- 자바스크립트 알고리즘 문제풀이/섹션6/5.쇠막대기.js
+++ 자바스크립트 알고리즘 문제풀이/섹션6/5.쇠막대기.js
@@ -1,8 +1,24 @@
 function solution(s) {
   let answer = 0;
-
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    console.log(stack);
+    if (s[i] === "(") {
+      stack.push("(");
+    } else {
+      stack.pop();
+      if (s[i - 1] === "(") {
+        //레이저인 경우
+        answer += stack.length;
+      } else {
+        //막대기의 끝인 경우
+        answer += 1;
+      }
+    }
+  }
   return answer;
 }
 
 let a = "()(((()())(())()))(())";
+// let a = "((())()()(()))";
 console.log(solution(a));
--- 자바스크립트 알고리즘 문제풀이/섹션6/6.공주구하기.js
+++ 자바스크립트 알고리즘 문제풀이/섹션6/6.공주구하기.js
@@ -1,5 +1,16 @@
 function solution(n, k) {
-  let answer;
+  let answer = 0;
+  let nArray = Array.from({ length: n }, (v, i) => i + 1);
+
+  while (nArray.length) {
+    for (let i = 1; i < k; i++) {
+      nArray.push(nArray.shift());
+    }
+    nArray.shift();
+    if (nArray.length === 1) {
+      answer = nArray.shift();
+    }
+  }
 
   return answer;
 }
