3dbb75871cf1a1724e812f6fa1520c1be4c6f506
yebun91 <yebun91@gmail.com>
2023-01-15 15:49:40 +0900

    섹션7 완료

--- 자바스크립트 알고리즘 문제풀이/섹션7/10.이분검색.js
+++ 자바스크립트 알고리즘 문제풀이/섹션7/10.이분검색.js
@@ -1,5 +1,22 @@
 function solution(target, arr) {
   let answer;
+  arr.sort((a, b) => a - b);
+  let lt = 0;
+  let rt = arr.length - 1;
+
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (arr[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (arr[mid] > target) {
+      rt = mid - 1;
+    } else {
+      lt = mid + 1;
+    }
+  }
+
+  console.log(arr);
 
   return answer;
 }
--- 자바스크립트 알고리즘 문제풀이/섹션7/9.결혼식.js
+++ 자바스크립트 알고리즘 문제풀이/섹션7/9.결혼식.js
@@ -1,5 +1,22 @@
 function solution(times) {
-  let answer = Number.MIN_SAFE_INTEGER;
+  let answer = 0;
+  let t_line = [];
+  for (let x of times) {
+    t_line.push([x[0], "s"]);
+    t_line.push([x[1], "e"]);
+  }
+  t_line.sort((a, b) => {
+    if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+    else return a[0] - b[0];
+  });
+  let cnt = 0;
+  for (let x of t_line) {
+    if (x[1] === "s") cnt++;
+    else cnt--;
+    answer = Math.max(answer, cnt);
+  }
+  console.log(t_line);
+
   return answer;
 }
 
