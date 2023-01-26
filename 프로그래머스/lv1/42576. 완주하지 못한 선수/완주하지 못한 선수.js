697c559c26db4dc78a5002cce17644e7dce7fd8e
yebun91 <65695703+yebun91@users.noreply.github.com>
2023-01-26 16:48:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 100.04 ms, Memory: 57.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,13 @@
+function solution(participant, completion) {
+    var answer = '';
+    let part = participant.sort();
+    let comp = completion.sort();
+    for(let i = 0; i<part.length; i++){
+        if(part[i] !== comp[i]){
+            answer += part[i];
+            break;
+        }
+    }
+    
+    return answer;
+}
