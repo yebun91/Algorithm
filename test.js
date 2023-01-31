9ee2b17e9d2428a58e2e5fadb317b0cf7dce7534
DESKTOP-TLT7564\choi yujin <yujin.choi@truabutment.com>
2023-01-31 16:48:23 +0900

    test.txt 추가

--- test.js
+++ test.js
@@ -1,32 +1,21 @@
-const [len, arr] = require("fs")
-  .readFileSync("test.txt")
+const input = require("fs")
+  .readFileSync("input.txt")
   .toString()
   .trim()
-  .split("\n");
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
 
-const solution = (start, arr) => {
-  let snakLen = start;
-  let sortArr = arr.sort((a, b) => a - b);
-  console.log(sortArr);
-
-  for (let x of sortArr) {
-    console.log(snakLen);
-
-    if (snakLen >= x) {
-      snakLen++;
-    } else {
-      break;
-    }
+const solution = () => {
+  let min = 999999;
+  while (input.length !== 0) {
+    const pop = input.pop();
+    const shift = input.shift();
+    min = Math.min(min, pop + shift);
   }
-
-  return snakLen;
+  return min;
 };
 
-console.log(
-  solution(
-    len.toString().trim().split(" ")[1],
-    arr.toString().trim().split(" ").map(Number)
-  )
-);
+console.log(solution());
 
 // https://pstudio411.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EB%B0%B1%EC%A4%80%EC%9D%84-%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0-%EC%9C%84%ED%95%9C-nodejs-%EB%9F%B0%ED%83%80%EC%9E%84-%EC%97%90%EB%9F%AC-%ED%95%B4%EA%B2%B0
