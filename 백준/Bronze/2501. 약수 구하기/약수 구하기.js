421159c98b6a8be7c0bffc3f9f865bb08016250f
yebun91 <65695703+yebun91@users.noreply.github.com>
2023-02-09 16:44:44 +0900

    [Bronze III] Title: 약수 구하기, Time: 140 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.js
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.js
@@ -7,8 +7,13 @@ const input = require("fs")
 
 const solution = (n, k) => {
   let result = [];
+  let count = 0;
   for (let i = 1; i <= n; i++) {
-    if (n % i === 0) result.push(i);
+    if (n % i === 0) {
+      result.push(i);
+      count++;
+      if (count >= k) break;
+    }
   }
   if (result[k - 1]) {
     return result[k - 1];
